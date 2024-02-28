65ff683e419b1f498ead05251f7701f5557805a6
Mubarak AlOraifan <154428345+Mubarak68@users.noreply.github.com>
2024-02-28 11:40:44 +0300

    Add files via upload
    Done with Basics,Loops,snapkit and ui tasks.
    Still with music task

---
+++ ios phase/MusicAppTask/MusicAppTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicAppTask
+//
+//  Created by Mubarak Aloraifan on 28/02/2024.
+//
+
+import UIKit
+import SnapKit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
